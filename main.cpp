134b7e5f4b34e22d81a4bae348f9457cd427c62a
wd230 <wd20206mori@outlook.jp>
2023-05-01 23:37:23 +0900

    カレントディレクトリ読み込み作成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <filesystem>
 #include "CsvData.h"
 
 int main() {
+    std::filesystem::path current_path = std::filesystem::current_path();
+    std::cout << "Current directory: " << current_path << std::endl;
     try {
         CsvData csv("./csv/result_3.csv");
     } catch (const std::exception& e) {
