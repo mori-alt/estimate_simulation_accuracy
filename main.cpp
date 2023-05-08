2769216046f8c3718390926d698488c7987df966
wd230 <wd20206mori@outlook.jp>
2023-05-08 16:44:41 +0900

    出力変更

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ int main() {
     std::filesystem::path current_path = std::filesystem::current_path();
     std::cout << "Current directory: " << current_path << std::endl;
     CsvData csv("./csv/result_3.csv");
-    std::cout  << csv.getFilePath() << std::endl;
 
 
     return 0;
