fc75f648285c71a8bd7c4ffe14211a231785ba3d
wd230 <wd20206mori@outlook.jp>
2023-05-08 16:33:39 +0900

    内容読み込み作成

--- CsvData.h
+++ CsvData.h
@@ -17,12 +17,22 @@ public:
     CsvData(std::string csv_path) {
 //        ファイルの読み込み
         file_path_ = csv_path;
-        std::cout << file_path_ << std::endl;
         std::ifstream file(file_path_, std::ios::in);
         if (!file.is_open()) {  // ちゃんと拾うかは置いといて失敗したらエラー投げさせる
             throw std::runtime_error("file cannot be opened");
         }
 
+        std::string str = "";
+        std::vector<std::string> lines;
+        while(getline(file, str)){
+            lines.push_back(str);
+            std::cout << str << std::endl;
+        }
+
+        std::cout << lines.size() << std::endl;
+        std::cout << lines[0] << std::endl;
+
+        file.close();
     }
 
     std::string getFilePath() const {
