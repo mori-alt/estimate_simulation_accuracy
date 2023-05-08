6cb79fe992552b6b7149a41bf75d5a761d38ea36
wd230 <wd20206mori@outlook.jp>
2023-05-08 17:30:52 +0900

    eye posの格納設定

--- CsvData.h
+++ CsvData.h
@@ -8,35 +8,67 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <array>
 
 class CsvData {
 private:
     std::string file_path_;
+    std::array<double, 3> eye_pos_;
+    std::array<double, 3> look_at_;
+    double dist_to_look_at_;
+    double camera_theta_;
+    double camera_phi_;
+    double camera_psi_;
+    double omega_;
 
 public:
     CsvData(std::string csv_path) {
-//        ファイルの読み込み
         file_path_ = csv_path;
         std::ifstream file(file_path_, std::ios::in);
         if (!file.is_open()) {  // ちゃんと拾うかは置いといて失敗したらエラー投げさせる
             throw std::runtime_error("file cannot be opened");
         }
 
+        // 全ての行の内容を読み込み
         std::string str = "";
         std::vector<std::string> lines;
         while(getline(file, str)){
             lines.push_back(str);
-            std::cout << str << std::endl;
         }
 
+        //　ヘッダ行の標準出力
         std::cout << lines.size() << std::endl;
-        std::cout << lines[0] << std::endl;
+        for(int i = 0; i < 7; i++){
+            std::cout << lines[i] << std::endl;
+        }
+
+        // eye posの格納
+        for(int i = 0; i < eye_pos_.size(); i++){
+            eye_pos_[i] = std::stod(split(lines[0], ',')[i + 1]);
+        }
 
         file.close();
     }
 
     std::string getFilePath() const {
         return file_path_;
+    }
+
+    std::array<double, 3> getEyePos() const {
+        return eye_pos_;
+    }
+
+    // csv読み込むとき用のカンマ区切り関数
+    std::vector<std::string> split(const std::string& input, char delimiter)
+    {
+        std::istringstream stream(input);
+
+        std::string field;
+        std::vector<std::string> result;
+        while (std::getline(stream, field, delimiter)) {
+            result.push_back(field);
+        }
+        return result;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ int main() {
     std::cout << "Current directory: " << current_path << std::endl;
     CsvData csv("./csv/result_3.csv");
 
+    std::cout << csv.getEyePos()[0] << std::endl;
+    std::cout << csv.getEyePos()[1] << std::endl;
+    std::cout << csv.getEyePos()[2] << std::endl;
+
+    std::cout << csv.getEyePos()[0] + csv.getEyePos()[1] << std::endl;
+
 
     return 0;
 }
