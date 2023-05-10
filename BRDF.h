2c4e31d7f63106e364bdd8d8f48298be6831db89
wd230 <wd20206mori@outlook.jp>
2023-05-10 12:21:23 +0900

    BRDF計算の引数調整，クラス化

--- BRDF.h
+++ BRDF.h
@@ -19,35 +19,37 @@ struct Light{ // 光源情報はまだ仮でおくだけだから適当に定義
 struct SurfaceGeometry{
     double amplitude;  // pdf出力の一番右の値にすればいい気がする（深さなので
     double pitch;  // 傷の付け方が直線的で，結局sinになっているけど詳しいこと分からないら適当に作ること
-    Eigen::Vector3d dv;
+    Eigen::Vector3d dv; // カメラの方向にあたる
 };
 
 struct Scene{
-    SurfaceGeometry geo;
-    Light light;
+    SurfaceGeometry geo; // pitchだけ自分で決めるけどそれ以外はcsvから取ること
+    Light light; // 自前で決めること
 };
 
 class BRDF{
 private:
     CsvData csv_;
+    Scene scene_;
 
 
 public:
+    // 必要な情報はここで読むこと
     BRDF(CsvData csv){
         csv_ = csv;
     }
 
-    double eval_sinusoidal_brdf( const double in_wave_length, const Eigen::Vector2d& in_random_st, const Eigen::Vector3d& in_dl, const Eigen::Vector3d& in_dv, const double amplitude, const double pitch )
+    double eval_sinusoidal_brdf( const Eigen::Vector2d& in_random_st )
     {
         using namespace std::complex_literals;
 
         const float g_c = 50.0; //[um]  表面構造の一辺の長さ
-        const auto k = 2.0 * M_PI / (in_wave_length * 1.0e-9);
-        const auto A = amplitude * 1.0e-9;
-        const auto L = pitch * 1.0e-9;
+        const auto k = 2.0 * M_PI / (scene_.light.wavelength * 1.0e-9);
+        const auto A = scene_.geo.amplitude * 1.0e-9;
+        const auto L = scene_.geo.pitch * 1.0e-9;
         const auto c = g_c * 1.0e-6;
 
-        const Eigen::Vector3d dh_bar = 0.5 * (in_dl + in_dv);
+        const Eigen::Vector3d dh_bar = 0.5 * (scene_.light.dl + scene_.geo.dv);
 
         const auto factor = ( dh_bar.z() == 0.0 ) ? c : ( std::exp( k * c * dh_bar.z() * 1.0i ) - std::exp( -k * c * dh_bar.z() * 1.0i ) ) / ( 2.0i * k * dh_bar.z() );
 
@@ -59,17 +61,17 @@ public:
         const auto iota_x1 = factor * _integral_x1_ * c;
         const auto iota_x2 = factor * _integral_x2_ * c;
 
-        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * in_dl.y() * in_dv.y() );
+        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * scene_.light.dl.y() * scene_.geo.dv.y() );
     }
 
 //    繰り返し処理の計算
-    double estimate_brdf(const int loop_freq, const double in_wavelength, const Eigen::Vector2d& in_random_st, const Eigen::Vector3d& dl, const Eigen::Vector3d& dv, const double amplitude, const double pitch){
+    double estimate_brdf(const int loop_freq){
         auto total_brdf_value = 0.0;
 #pragma omp parallel for
         for(int i = 0; i < loop_freq; i++){
             Eigen::Vector2d brdf_st;
             brdf_st << randomMT(), randomMT();
-            const auto brdf_value = eval_sinusoidal_brdf(in_wavelength, brdf_st, dl, dv, amplitude, pitch);
+            const auto brdf_value = eval_sinusoidal_brdf( brdf_st );
             total_brdf_value += brdf_value;
         }
         auto brdf = total_brdf_value / loop_freq;
@@ -83,6 +85,10 @@ public:
 
     void setCsvData(CsvData csv){
         csv_ = csv;
+    }
+
+    const Scene &getScene() const {
+        return scene_;
     }
 
 };
