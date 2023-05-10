6da99559f29d42d0651dc4aac21728aa8c54699f
wd230 <wd20206mori@outlook.jp>
2023-05-10 17:24:54 +0900

    必要なメンバ変数を決定

--- BRDF.h
+++ BRDF.h
@@ -14,19 +14,64 @@
 // fixme なんかcmathの定数が呼び出せないからごり押し解決
 const double M_PI = 3.141592653589793;
 
-class Light{
+class BRDF{
 private:
+    // 光源情報
     double wavelength_;
     Eigen::Vector3d dl_;
 
-public:
-    Light(){
+    // 表面構造
+    double amplitude_;  // pdf出力の一番右の値にすればいい気がする（深さなので
+    double pitch_;  // 傷の付け方が直線的で，結局sinになっているらしいけど詳しいこと分からないら適当に作ること
+    Eigen::Vector3d dv_; // カメラの方向にあたる
 
+    // 物体の回転を追いかけるベクトル
+    // 実装は物体表面ではなくカメラと光源が一緒に動く感じにする
+    std::vector<double> rot_angle_;
+
+public:
+    BRDF(){}
+    BRDF(const double wavelength, const  Eigen::Vector3d& dl, const  double amplitude, const  double pitch, const Eigen::Vector3d& dv, const std::vector<double>& rot_angle)
+    : wavelength_(wavelength), dl_(dl.normalized()), amplitude_(amplitude), pitch_(pitch), dv_(dv.normalized()), rot_angle_(rot_angle){}
+
+    double eval_sinusoidal_brdf( const Eigen::Vector2d& in_random_st ) const
+    {
+        using namespace std::complex_literals;
+
+        const float g_c = 50.0; //[um]  表面構造の一辺の長さ
+        const auto k = 2.0 * M_PI / (wavelength_ * 1.0e-9);
+        const auto A = amplitude_ * 1.0e-9;
+        const auto L = pitch_ * 1.0e-9;
+        const auto c = g_c * 1.0e-6;
+
+        const Eigen::Vector3d dh_bar = 0.5 * (dl_ + dv_);
+
+        const auto factor = ( dh_bar.z() == 0.0 ) ? c : ( std::exp( k * c * dh_bar.z() * 1.0i ) - std::exp( -k * c * dh_bar.z() * 1.0i ) ) / ( 2.0i * k * dh_bar.z() );
+
+        // 計算する点の指定　辺の長さで決めてる
+        const auto _x1 = ( in_random_st.x() - 0.5 ) * c;
+        const auto _x2 = ( in_random_st.y() - 0.5 ) * c;
+        const auto _integral_x1_ = std::exp( -2.0i * k * ( _x1 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x1/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x1/L )*dh_bar.x()/L + dh_bar.y() );
+        const auto _integral_x2_ = std::exp( -2.0i * k * ( _x2 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x2/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x2/L )*dh_bar.x()/L + dh_bar.y() );
+        const auto iota_x1 = factor * _integral_x1_ * c;
+        const auto iota_x2 = factor * _integral_x2_ * c;
+
+        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * dl_.y() * dv_.y() );
     }
 
-    Light(double wavelength, const Eigen::Vector3d& dl){
-        wavelength_ = wavelength;
-        dl_ = dl.normalized();
+//    繰り返し処理の計算
+    double estimate_brdf(const int loop_freq) const {
+        auto total_brdf_value = 0.0;
+#pragma omp parallel for
+        for(int i = 0; i < loop_freq; i++){
+            Eigen::Vector2d brdf_st;
+            brdf_st << randomMT(), randomMT();
+            const auto brdf_value = eval_sinusoidal_brdf( brdf_st );
+            total_brdf_value += brdf_value;
+        }
+        auto brdf = total_brdf_value / loop_freq;
+
+        return brdf;
     }
 
     double getWavelength() const {
@@ -43,24 +88,6 @@ public:
 
     void setDl(const Eigen::Vector3d &dl) {
         dl_ = dl;
-    }
-};
-
-class SurfaceGeometry{
-private:
-    double amplitude_;  // pdf出力の一番右の値にすればいい気がする（深さなので
-    double pitch_;  // 傷の付け方が直線的で，結局sinになっているらしいけど詳しいこと分からないら適当に作ること
-    Eigen::Vector3d dv_; // カメラの方向にあたる
-
-public:
-    SurfaceGeometry(){
-
-    }
-
-    SurfaceGeometry(double amplitude, double pitch, Eigen::Vector3d dv){
-        amplitude_ = amplitude;
-        pitch_ = pitch;
-        dv_ = dv.normalized();
     }
 
     double getAmplitude() const {
@@ -86,80 +113,6 @@ public:
     void setDv(const Eigen::Vector3d &dv) {
         dv_ = dv;
     }
-};
-
-class BRDF{
-private:
-    Light light_;
-    SurfaceGeometry geo_;
-
-
-public:
-    // 必要な情報はここで読むこと
-    BRDF(){
-        // 光源情報の作成
-        Light light{375, {0, 1, 1}};
-        light_ = light;
-
-
-    }
-
-    double eval_sinusoidal_brdf( const Eigen::Vector2d& in_random_st )
-    {
-        using namespace std::complex_literals;
-
-        const float g_c = 50.0; //[um]  表面構造の一辺の長さ
-        const auto k = 2.0 * M_PI / (light_.getWavelength() * 1.0e-9);
-        const auto A = geo_.getAmplitude() * 1.0e-9;
-        const auto L = geo_.getPitch() * 1.0e-9;
-        const auto c = g_c * 1.0e-6;
-
-        const Eigen::Vector3d dh_bar = 0.5 * (light_.getDl() + geo_.getDv());
-
-        const auto factor = ( dh_bar.z() == 0.0 ) ? c : ( std::exp( k * c * dh_bar.z() * 1.0i ) - std::exp( -k * c * dh_bar.z() * 1.0i ) ) / ( 2.0i * k * dh_bar.z() );
-
-        // 計算する点の指定　辺の長さで決めてる
-        const auto _x1 = ( in_random_st.x() - 0.5 ) * c;
-        const auto _x2 = ( in_random_st.y() - 0.5 ) * c;
-        const auto _integral_x1_ = std::exp( -2.0i * k * ( _x1 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x1/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x1/L )*dh_bar.x()/L + dh_bar.y() );
-        const auto _integral_x2_ = std::exp( -2.0i * k * ( _x2 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x2/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x2/L )*dh_bar.x()/L + dh_bar.y() );
-        const auto iota_x1 = factor * _integral_x1_ * c;
-        const auto iota_x2 = factor * _integral_x2_ * c;
-
-        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * light_.getDl().y() * geo_.getDv().y() );
-    }
-
-//    繰り返し処理の計算
-    double estimate_brdf(const int loop_freq){
-        auto total_brdf_value = 0.0;
-#pragma omp parallel for
-        for(int i = 0; i < loop_freq; i++){
-            Eigen::Vector2d brdf_st;
-            brdf_st << randomMT(), randomMT();
-            const auto brdf_value = eval_sinusoidal_brdf( brdf_st );
-            total_brdf_value += brdf_value;
-        }
-        auto brdf = total_brdf_value / loop_freq;
-
-        return brdf;
-    }
-
-    const Light &getLight() const {
-        return light_;
-    }
-
-    void setLight(const Light &light) {
-        light_ = light;
-    }
-
-    const SurfaceGeometry &getGeo() const {
-        return geo_;
-    }
-
-    void setGeo(const SurfaceGeometry &geo) {
-        geo_ = geo;
-    }
-
 };
 
 #endif //ESTIMATE_SIMULATION_ACCURACY_BRDF_H
--- main.cpp
+++ main.cpp
@@ -4,11 +4,27 @@
 #include "CsvData.h"
 #include "BRDF.h"
 
+
 int main() {
     std::filesystem::path current_path = std::filesystem::current_path();
     std::cout << "Current directory: " << current_path << std::endl;
     CsvData csv("./csv/result_3.csv");
-    BRDF brdf(csv);
+
+    std::vector<double> rot_angle;
+    BRDF brdf(375, {1, 1, 1}, 200, 300, {-1, 1, 1}, rot_angle);
+
+
+
+    std::string str = "[p0050nm_00deg_d074nm-theta]";
+    std::vector<double> numbers = extractNumbers(csv.getHeader()[2]);
+
+    std::cout << csv.getRotAngle()[0] << std::endl;
+    std::cout << csv.getRotAngle()[1] << std::endl;
+    std::cout << csv.getRotAngle()[2] << std::endl;
+
+    std::cout << csv.read_camera_solid_angles(1)[1][0] - csv.read_camera_solid_angles(1)[1][1] << std::endl;
+    std::cout << csv.read_camera_solid_angles(1)[1][1] - csv.read_camera_solid_angles(1)[1][2] << std::endl;
+    std::cout << csv.read_camera_solid_angles(1)[1][2] - csv.read_camera_solid_angles(1)[1][3] << std::endl;
 
 
     return 0;
