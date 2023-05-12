d09e8ce90f33048b96b3d6665d55f920d987842f
wd230 <wd20206mori@outlook.jp>
2023-05-12 19:03:39 +0900

    add wavelength to color function

---
+++ spectra/spectra_data.h
@@ -0,0 +1,10 @@
+#ifndef __SPECTRUM_DATA_H__
+#define __SPECTRUM_DATA_H__
+
+#include <Eigen/Dense>
+#include <vector>
+
+void getXYZForSpectraWindow( const unsigned long in_Lambda, const unsigned long in_DeltaLambda, Eigen::Vector3d& out_XYZ );
+void convertSpectraData2RGB( Eigen::Vector3d& out_RGB, const int in_NumSpect, const std::vector<Eigen::Vector3d>& in_Spectra2XYZ, const double* in_SpectraData, const double in_Ratio );
+
+#endif
