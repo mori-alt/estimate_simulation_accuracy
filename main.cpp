34f58f695415cffc3e434d6adb50939a6b09b046
wd230 <wd20206mori@outlook.jp>
2023-05-19 15:51:44 +0900

    波長色変換関数の呼び出し

--- main.cpp
+++ main.cpp
@@ -7,7 +7,26 @@
 
 
 int main() {
+    // set scene info
+    const auto DL = 25;
+    const auto NSPECT = 16;
+    const int WAVELENGTHS[NSPECT] = { 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700, 725, 750 };
+    const auto lgith_intensity = 0.25;
     const double wavelength = 375;
+    std::vector<int> wavelengths;
+    std::vector<Eigen::Vector3d> spectra2XYZ_conversion;
+
+    wavelengths.clear();
+    spectra2XYZ_conversion.clear();
+
+    // set XYZ_data
+    for(auto i = 0; i < NSPECT; ++i){
+        wavelengths.push_back( WAVELENGTHS[i]);
+        Eigen::Vector3d XYZ_data;
+        getXYZForSpectraWindow(WAVELENGTHS[i], DL, XYZ_data);
+        spectra2XYZ_conversion.push_back( XYZ_data);
+    }
+
     const double pitch = 600; // todo ask material surface situation
     Eigen::Vector3d dl(0, 1, 1);
     CsvData csv("./csv/result_3.csv");
@@ -31,7 +50,7 @@ int main() {
 
     csv.update_RGB(RGB, 4);
 
-    // calculate all situation
+    // calculate all situation calc all
     for(int i = 0; i < 74; ++i){
         // calculate brdf
         BRDF brdf2(wavelength, dl, csv.getSurfaceGeo(i)[2], pitch, csv.getCameraPos(), csv.getRotAngle());
@@ -39,7 +58,6 @@ int main() {
 
         // convert2RGB
         std::vector<Eigen::Vector3d> rgb;
-        for(double wavelength : brdfs) rgb.push_back(convertSpectraData2RGB());
 
         // update csv_data
         csv.update_RGB(rgb, i);
