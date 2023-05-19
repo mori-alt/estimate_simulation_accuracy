7b0a73c62c0da2acfbf6c880f66f617638e6fbc1
wd230 <wd20206mori@outlook.jp>
2023-05-19 17:14:20 +0900

    BRDF関数の実装　静的メンバに変更

--- BRDF.h
+++ BRDF.h
@@ -54,21 +54,27 @@ public:
     BRDF(const double wavelength, const  Eigen::Vector3d& dl, const  double amplitude, const  double pitch, const Eigen::Vector3d& dv, const std::vector<double>& rot_angle)
     : wavelength_(wavelength), dl_(dl.normalized()), amplitude_(amplitude), pitch_(pitch), dv_(dv.normalized()), rot_angle_(rot_angle){}
 
-    double eval_sinusoidal_brdf( const Eigen::Vector2d& in_random_st ) const
+    static double eval_sinusoidal_brdf( const double _in_wave_length, const Eigen::Vector2d& in_random_st, const Eigen::Vector3d& in_dl, const Eigen::Vector3d& in_dv, const double amplitude, const double pitch )
     {
         using namespace std::complex_literals;
 
-        const float g_c = 50.0; //[um]  表面構造の一辺の長さ
-        const auto k = 2.0 * M_PI / (wavelength_ * 1.0e-9);
-        const auto A = amplitude_ * 1.0e-9;
-        const auto L = pitch_ * 1.0e-9;
-        const auto c = g_c * 1.0e-6;
+        const auto in_wave_length = _in_wave_length * 1.0e-9;
+        const auto k = 2.0 * M_PI / in_wave_length;
+        const auto A = amplitude * 1.0e-9;
+        const auto L = pitch * 1.0e-9;
+        const auto c = 50 * 1.0e-6;
 
-        const Eigen::Vector3d dh_bar = 0.5 * (dl_ + dv_);
+        const Eigen::Vector3d dh_bar = ( in_dl + in_dv ) * 0.5;
 
         const auto factor = ( dh_bar.z() == 0.0 ) ? c : ( std::exp( k * c * dh_bar.z() * 1.0i ) - std::exp( -k * c * dh_bar.z() * 1.0i ) ) / ( 2.0i * k * dh_bar.z() );
 
-        // 計算する点の指定　辺の長さで決めてる
+        //the following way is probably buggy, as E[x^2] != E[x]^2
+        //const auto _x = ( randomMT() - 0.5 ) * c;
+        //const auto _integral_ = std::exp( -2.0i * k * ( _x * dh_bar.x() + A * std::sin( 2.0*std::numbers::pi*_x/L ) * dh_bar.y() ) ) * ( -2.0*std::numbers::pi*A*std::cos( 2.0*std::numbers::pi*_x/L )*dh_bar.x()/L + dh_bar.y() );
+        //const auto iota = factor * _integral_ * c;
+        //return k * k * ( std::conj(iota) * iota ).real() / ( 4.0 * std::numbers::pi * std::numbers::pi * c * c * in_dl.y() * in_dv.y() );
+
+        //this is possibly correct
         const auto _x1 = ( in_random_st.x() - 0.5 ) * c;
         const auto _x2 = ( in_random_st.y() - 0.5 ) * c;
         const auto _integral_x1_ = std::exp( -2.0i * k * ( _x1 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x1/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x1/L )*dh_bar.x()/L + dh_bar.y() );
@@ -76,7 +82,34 @@ public:
         const auto iota_x1 = factor * _integral_x1_ * c;
         const auto iota_x2 = factor * _integral_x2_ * c;
 
-        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * dl_.y() * dv_.y() );
+        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * in_dl.y() * in_dv.y() );
+    }
+
+    double eval_sinusoidal_brdf( const Eigen::Vector2d& in_random_st ) const
+    {
+//        using namespace std::complex_literals;
+//
+//        const float g_c = 50.0; //[um]  表面構造の一辺の長さ
+//        const auto k = 2.0 * M_PI / (wavelength_ * 1.0e-9);
+//        const auto A = amplitude_ * 1.0e-9;
+//        const auto L = pitch_ * 1.0e-9;
+//        const auto c = g_c * 1.0e-6;
+//
+//        const Eigen::Vector3d dh_bar = 0.5 * (dl_ + dv_);
+//
+//        const auto factor = ( dh_bar.z() == 0.0 ) ? c : ( std::exp( k * c * dh_bar.z() * 1.0i ) - std::exp( -k * c * dh_bar.z() * 1.0i ) ) / ( 2.0i * k * dh_bar.z() );
+//
+//        // 計算する点の指定　辺の長さで決めてる
+//        const auto _x1 = ( in_random_st.x() - 0.5 ) * c;
+//        const auto _x2 = ( in_random_st.y() - 0.5 ) * c;
+//        const auto _integral_x1_ = std::exp( -2.0i * k * ( _x1 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x1/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x1/L )*dh_bar.x()/L + dh_bar.y() );
+//        const auto _integral_x2_ = std::exp( -2.0i * k * ( _x2 * dh_bar.x() + A * std::sin( 2.0*M_PI*_x2/L ) * dh_bar.y() ) ) * ( -2.0*M_PI*A*std::cos( 2.0*M_PI*_x2/L )*dh_bar.x()/L + dh_bar.y() );
+//        const auto iota_x1 = factor * _integral_x1_ * c;
+//        const auto iota_x2 = factor * _integral_x2_ * c;
+
+        return BRDF::eval_sinusoidal_brdf(wavelength_, in_random_st, dl_, dv_, amplitude_, pitch_);
+
+//        return k * k * ( iota_x1 * std::conj(iota_x2) ).real() / ( 4.0 * M_PI * M_PI * c * c * dl_.y() * dv_.y() );
     }
 
     // calc expected value
@@ -86,7 +119,7 @@ public:
         for(int i = 0; i < loop_freq; i++){
             Eigen::Vector2d brdf_st;
             brdf_st << randomMT(), randomMT();
-            const auto _brdf_value = eval_sinusoidal_brdf( brdf_st );
+            const auto _brdf_value = eval_sinusoidal_brdf(wavelength_, brdf_st, dl_, dv_, amplitude_, pitch_);
             total_brdf_value += _brdf_value;
         }
         auto brdf_exp_value = total_brdf_value / loop_freq;
@@ -104,6 +137,10 @@ public:
         }
 
         return brdfs;
+    }
+
+    std::vector<double> eval_brdf_each_rot_angle() {
+
     }
 
     // for debug
