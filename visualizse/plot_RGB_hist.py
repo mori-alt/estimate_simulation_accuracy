53404ee20c6340f590817806d9e609e826c60e59
morimoto <yuta212211@ezweb.ne.jp>
2023-06-21 17:30:34 +0900

    refactor : fix code layout

--- visualizse/plot_RGB_hist.py
+++ visualizse/plot_RGB_hist.py
@@ -9,6 +9,7 @@ import csv
 # データの一覧取得
 files = glob.glob('../cmake-build-release/output_csv/*')
 files = [x.replace('\\', '/') for x in files]
+
 print('input these files')
 print(files)
 
@@ -18,7 +19,6 @@ for file in files:
     data_list.append(pd.read_csv(file, header=7))
 
 # get header
-# data = pd.read_csv(files[3], header=7)
 names = data_list[0].columns.values
 print(names)
 
