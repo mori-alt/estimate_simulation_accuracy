793829bd2d5c96e141d00bf4283fc8f6b6bf802b
morimoto <yuta212211@ezweb.ne.jp>
2023-06-21 19:21:39 +0900

    feat : plot histgram

--- visualizse/plot_RGB_hist.py
+++ visualizse/plot_RGB_hist.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 import glob
 import csv
+import seaborn as sns
 
 # データの一覧取得
 files = glob.glob('../cmake-build-release/output_csv/*')
@@ -19,11 +20,60 @@ for file in files:
     data_list.append(pd.read_csv(file, header=7))
 
 # get header
-names = data_list[0].columns.values
-print(names)
+names = data_list[3].columns.values
 
 # set page config
 st.set_page_config(layout="wide")
 
 def genRGBHist(surface_index):
     print('write ' + str(surface_index) + ' surface index')
+    r = []
+    g = []
+    b = []
+
+    fig1 = plt.figure()
+
+    plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.1, hspace=0.6)
+
+    # set plot area
+    ax = fig1.add_subplot(1, 1, 1)
+    ax.set_aspect('equal')
+    n = 2 + 5 * surface_index
+
+    for movie_num in range(len(files)):
+        d = pd.read_csv(files[movie_num], header=7)
+
+        with open(files[movie_num], 'r') as file:
+            reader = csv.reader(file)
+            data = [row[1] for idx, row in enumerate(reader) if idx == 3]
+
+        # get RGB
+        C = np.array([d.iloc[:, n + 2].values, d.iloc[:, n + 3].values, d.iloc[:, n + 4].values]) / 255.0
+        C = C.transpose()
+
+        for rgb in C:
+            r.append(rgb[0])
+            g.append(rgb[1])
+            b.append(rgb[2])
+
+    # set title
+    _title = str(int(names[n][3:7])) + 'nm/' + str(int(names[n][10:12])) + '/' + str(int(names[n][17:20])) + 'nm'
+    print(_title)
+
+    R = np.array(r) * 10000
+    G = np.array(g)
+    B = np.array(b)
+    print('size')
+    print(R.size)
+    print('min')
+    print(R.min())
+    print('max')
+    print(R.max())
+    print('scale')
+    print(R.max() - R.min())
+
+    plt.hist(R)
+    plt.show()
+
+
+genRGBHist(0)
